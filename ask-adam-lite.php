3879f743e0494fc7644ad95b3dd813873aa36131
jackofall1232 <jackofall1232@gmail.com>
2025-09-25 18:59:35 -0400

    Update ask-adam-lite.php

--- ask-adam-lite.php
+++ ask-adam-lite.php
@@ -6,7 +6,7 @@
  * Author:            Ask Adam
  * Text Domain:       ask-adam-lite
  * Requires at least: 5.8
- * Tested up to:      6.7
+ * Tested up to:      6.8
  * Requires PHP:      7.4
  * License:           GPLv2 or later
  */
