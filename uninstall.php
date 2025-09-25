258670b2876932966a0b42a9747653c0348d8cd5
jackofall1232 <jackofall1232@gmail.com>
2025-09-25 10:06:21 -0400

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,18 +1,24 @@
 <?php
 // If uninstall not called from WordPress, exit.
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) { exit; }
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit;
+}
 
-// Remove Lite options (keep content alone).
+// Remove Lite options (keep content untouched).
 $opts = [
-  'adam_lite_widget_settings',
-  'adam_lite_api_settings',
-  'adam_lite_theme_colors',
-  'adam_lite_kb_settings',
-  'adam_lite_api_keys',
+    'aalite_widget_settings',
+    'aalite_api_settings',
+    'aalite_kb_settings',
 ];
 
+// Delete options for single-site.
 foreach ( $opts as $k ) {
     delete_option( $k );
 }
 
-// If you ever store sitewide (multisite), also delete_site_option( $k ).
+// Also clean up for multisite.
+if ( is_multisite() ) {
+    foreach ( $opts as $k ) {
+        delete_site_option( $k );
+    }
+}
